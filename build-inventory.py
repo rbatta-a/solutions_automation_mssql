70cde65d44927126207cd8c3eb47d038f2bc5d99
vkulari <124032976+vkulari@users.noreply.github.com>
2024-12-05 17:35:11 +0530

    Updated the condition for Veeam with right approirate

--- build-inventory.py
+++ build-inventory.py
@@ -62,7 +62,7 @@ The abopve Veeam setup is done
 '''
 
 
-if sol == 'veeam-server':
+if sol == 'veeam-linux-servers':
     append_ip_to_hosts_in_veeam_linux_server(ip_addresses=ips)
 if sol == 'MSSQL': #or 'MSSQLDC':
     print(sol)
