2c739d3d39ac398df14c2955cf9ddd2c9ba582a5
annamaria-george <amariageorge@purestorage.com>
2024-12-09 20:16:46 +0530

    Update build-inventory.py

--- build-inventory.py
+++ build-inventory.py
@@ -27,8 +27,8 @@ if sol == 'MSSQLDC_Test':
     with open(filename,'w') as fh:
         fh.write("[win]\n")
         for name in names:
-            fh.write(name.rstrip().split('.')[0]+ '.lab.local' + '\n')
-            # fh.write(name.rstrip().split('.')[0]+ '.puretec.purestorage.com' + '\n')
+            # fh.write(name.rstrip().split('.')[0]+ '.lab.local' + '\n')
+            fh.write(name.rstrip().split('.')[0]+ '.puretec.purestorage.com' + '\n')
         fh.write("[win:vars]\n")
         fh.write("ansible_user=administrator\n")
         fh.write("ansible_password=VMware1!\n")
