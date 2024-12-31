16b53a8500274e296920b4718b9b9756ed31f1bb
annamaria-george <amariageorge@purestorage.com>
2024-12-31 11:52:04 +0530

    Update main.tf

--- modules/MSSQL_Test/main.tf
+++ modules/MSSQL_Test/main.tf
@@ -1,5 +1,5 @@
 module "vm" {
-    source = "../basemodule-windows-1disk"
+    source = "../basemodule-windows"
     vmware_os_template               = var.vmware_os_template
     osguest_id                       = var.osguest_id
     internal_domain                  = var.internal_domain
