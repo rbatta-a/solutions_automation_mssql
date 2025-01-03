9066cd61a51892d6c7094c71f3500b99dfeefb63
annamaria-george <amariageorge@purestorage.com>
2025-01-03 19:53:38 +0530

    Update script.ps1

--- ansible/roles/mssql-test/files/script.ps1
+++ ansible/roles/mssql-test/files/script.ps1
@@ -1,3 +1,3 @@
 Import-Module 'C:\Program Files\Pure Storage\D.O.E.S\DOES.Powershell.dll'
 
-Add-DataEngine -DatabaseType MicrosoftSQL -Hostname localhost -DatabaseName db1 -Amount 1 -Unit Terabytes -NumberOfThreads 4 -UserName administrator -Password VMware1!
+Add-DataEngine -DatabaseType MicrosoftSQL -Hostname localhost -DatabaseName db1 -Amount 500 -Unit Gigabytes -NumberOfThreads 4 -UserName administrator -Password VMware1!
